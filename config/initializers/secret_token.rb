ab3c9f11c3fcc7395f45a6ec9dc5dc857b76148a
hide2 <hideto.bj@gmail.com>
2012-02-01 17:39:11 +0800

    hi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hi2De::Application.config.secret_token = 'c17709c5cc27af96a0e442fd4001b37787b9dd5cbefbc02e624951f8423fa42a38d63e8a2059ad05cb7ec2bacd28bf0e8f729d8c904460c095de99e1cb3cfec6'
